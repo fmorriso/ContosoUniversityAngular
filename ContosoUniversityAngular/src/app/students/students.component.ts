66d4eaa6a9d32d68d44b2829d88c91e43c41f727
Fred Morrison <fpmorrison@verizon.net>
2017-07-11 21:23:37 -0400

    Students page uses a Progress Kendo UI Angular grid

--- ContosoUniversityAngular/src/app/students/students.component.ts
+++ ContosoUniversityAngular/src/app/students/students.component.ts
@@ -1,31 +1,32 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { Observable }        from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
-import { Student }           from '../models/student';
-import { StudentsService }   from './students.service';
+import { Student } from '../models/student';
+import { StudentsService } from './students.service';
 
 @Component({
-  selector: 'app-students',
-  templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+	selector: 'app-students',
+	templateUrl: './students.component.html',
+	styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent implements OnInit {
 
-  students: Observable<Student[]>;
+	private compName: string = 'StudentsComponent';
+	students: Observable<Student[]>;
 
-  constructor(private studentsService: StudentsService) {
-    //console.log('StudentsComponent - constructor');
-  }
+	constructor(private studentsService: StudentsService) {
+		console.log(`${this.compName} - constructor`);
+	}
 
-  ngOnInit() {
-    //console.log('StudentsComponent - ngOnInit - 1.1');
-    this.getStudents();
-    //console.log('StudentsComponent - ngOnInit - 2.1');
-  }
+	ngOnInit() {
+		//console.log(`${this.compName}- ngOnInit - 1.1');
+		this.getStudents();
+		console.log(`${this.compName} - ngOnInit - `);
+	}
 
-  getStudents() {
-    //console.log('StudentsComponent - getStudents - 1');
-    this.students = this.studentsService.getStudents();
-    //console.log('StudentsComponent - getStudents - 2');
-  }
+	getStudents() {
+		//console.log(`${this.compName}- getStudents - 1`);
+		this.students = this.studentsService.getStudents();
+		console.log(`${this.compName} - getStudents - 2`);
+	}
 }
