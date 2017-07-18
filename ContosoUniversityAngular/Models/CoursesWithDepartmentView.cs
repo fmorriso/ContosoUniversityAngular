88c6a4b1f47e72907194b3f68427d0662d4d49d2
Fred Morrison <fpmorrison@verizon.net>
2017-07-18 10:18:10 -0400

    Changed Courses grid to use a View instead of a combination of Courses, Include() and Select() to avoid run-time error when trying to filter or sort on the DepartmentName column of the grid.

---
+++ ContosoUniversityAngular/Models/CoursesWithDepartmentView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversityAngular.Models
+{
+	[Table("CoursesWithDepartmentView")]
+	public class CoursesWithDepartmentView
+    {
+		[Key]
+		public int CourseID { get; set; }
+		public string Title { get; set; }
+		public int Credits { get; set; }
+
+	    public decimal Budget { get; set; }
+
+		public string DepartmentName { get; set; }
+
+	}
+}
